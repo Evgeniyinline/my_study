6133241bc830c08fee7c19e0d4deb10e1cd01817
Evgeniy <longlimp@gmail.com>
2026-02-17 18:01:35 +0300

    test-commit-1

--- example.spec.ts
+++ example.spec.ts
@@ -8,7 +8,7 @@ test('has title', async ({ page }) => {
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://calc-dev.v04.dev/auth');
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
