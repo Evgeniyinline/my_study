5e19b9af1a74c47b95775aea1bfcb1172d44f88e
Evgeniy Orlov <longlimp@gmail.com>
2026-02-24 19:09:24 +0300

    add navigation test

--- tests/calculator.auth.spec.ts
+++ tests/calculator.auth.spec.ts
@@ -1,17 +1,31 @@
 import { test, expect } from "@playwright/test";
 
-const url = 'https://calc-dev.v04.dev/orders/pro';
+const url = 'https://calc-dev.v04.dev';
 
-// смена типов калькулятора
-test('change calculator to pro or base', async ({ page }) => {
+// проверка всех разделов навигации
+test('check all navigation sections', async ({ page }) => {
+
+  const locator = 'h1'
 
   await page.goto(url);
   await expect(page.getByTestId('pro-calculator-content')).toBeVisible();
   await page.getByTestId('header-nav-link-orders-base').click();
   await expect(page.getByTestId('base-calculator-switch-confirm-modal-content')).toBeVisible();
   await page.getByTestId('base-calculator-switch-confirm-continue').click();
-  await expect(page.getByTestId('base-calculator-content').locator('h1').first()).toHaveText('Base');
-  await expect(page).toHaveURL('https://calc-dev.v04.dev/orders/base');
+  await expect(page.getByTestId('base-calculator-content').locator(locator).first()).toHaveText('Base');
+  await expect(page).toHaveURL(url + '/orders/base');
+  await page.getByTestId('header-nav-link-analytics').click();
+  await expect(page).toHaveURL(url + '/analytics');
+  await expect(page.getByTestId('analytics-page').locator(locator)).toHaveText('Аналитика продукта');
+  await page.getByTestId('header-nav-link-nomenclatures').click();
+  await expect(page).toHaveURL(url + '/nomenclatures');
+  await expect(page.getByTestId('nomenclatures-page').locator(locator)).toHaveText('Номенклатуры');
+  await page.getByTestId('header-nav-link-users').click();
+  await expect(page).toHaveURL(url + '/users');
+  await expect(page.getByTestId('users-page').locator(locator)).toHaveText('Пользователи');
+  await page.getByTestId('app-logo-link').click();
+  await expect(page).toHaveURL(url + '/orders/pro');
+  await expect(page.getByTestId('pro-calculator-content').locator(locator).first()).toHaveText('Pro');
 
 });
 
